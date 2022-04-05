49d0e8bc48fe7198fd8c963077af6e28093e8dd6
Ashok Bajiya <88234185+ashok-abjima@users.noreply.github.com>
2022-04-05 14:38:05 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Test
+{
+    int i =0;
+    static int j = 0;
+
+    public void SetData()
+    {
+        i++;
+        j++;
+    }
+    public void GetData()
+    {
+        Console.WriteLine("{0} {1}",i,j);
+    }
+
+}
+class Program
+{
+    static void Main()
+    {
+        Test ob = new Test();
+       
+
+
+        Test ob1 = new Test();
+        ob1.SetData();
+        ob.GetData();
+        ob1.GetData();
+
+        Console.ReadKey();
+    }
+}
